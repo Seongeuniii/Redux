56ae4477a88e77ed9d150866320f35c76eab3ac6
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-06-09 16:20:18 +0900

    스토어 생성, react-redux: Provider 컴포넌트 사용

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 리액트 컴포넌트에서 스토어를 사용하려면 Provider 컴포넌트로 감싸준다.
+// Provider 컴포넌트를 사용할 때 store를 props로 전달해 주어야 한다.
